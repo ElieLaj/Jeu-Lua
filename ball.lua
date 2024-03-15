2d225b777d11f4b570ec0646eb33340c7099d4f9
Azene <elie.lajoinie@supdevinci-edu.fr>
2024-03-15 14:46:18 +0100

    Reset et Hit

--- ball.lua
+++ ball.lua
@@ -2,16 +2,29 @@ local Player = require("player")
 
 function Ball(x, y, width, height, speed, color)
     local ball = Player(x, y, width, height, speed, color)
-    ball.status = 0
-    ball.setStatus = function(self, status, color)
-        self.status = status
-        ball.setColor(self, color)
-        ball.setSpeed(self, self.speed * -1)
+    ball.status = { x = 1, y = 1 }
+
+    ball.hit = function(self, color)
+        self.status.x = ball.status.x * -1
+        ball.color = color
     end
 
     ball.getStatus = function(self)
         return self.status
     end
+
+    ball.move = function(self, dt)
+        self.x = self.x + self.speed * self.status.x * dt
+        self.y = self.y + self.speed * self.status.y * dt
+    end
+
+    ball.reset = function(self)
+        self.x = self.baseX
+        self.y = self.baseY
+        self.speed = self.baseSpeed
+        self.status.x = self.status.x * -1
+    end
+    return ball
 end
 
 return Ball
